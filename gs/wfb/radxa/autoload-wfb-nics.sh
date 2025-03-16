e7c035ff154544c132a6ddab8551698bdfd400ef
snokvist <joakim.snokvist@gmail.com>
2025-03-16 17:37:38 +0100

    Update autoload-wfb-nics.sh

--- gs/wfb/radxa/autoload-wfb-nics.sh
+++ gs/wfb/radxa/autoload-wfb-nics.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Initialize log file and type variables
+# Initialize log file, type variables, and aggregator flag
 LOG_FILE=""
 TYPE=""
-AGGREGATOR_ENABLED=true  # Default value
+AGGREGATOR_ENABLED=false  # Default value
 
 # Parse arguments
 while [[ "$#" -gt 0 ]]; do
@@ -30,21 +30,10 @@ matching_interfaces=""
 # List all network interfaces and filter only wireless ones
 for iface in /sys/class/net/*; do
     iface_name=$(basename $iface)
-    # Check if the interface is wireless
-    if [ -d "/sys/class/net/$iface_name/wireless" ]; then
-        # Check if the interface is usb
-        if [[ "$(readlink -f /sys/class/net/$iface_name/device/subsystem)" == *"usb"* ]]; then
-            # Append the interface name to the list
-            matching_interfaces+="$iface_name "
-        else
-            # Check if the interface name is not wlan0
-            if [[ "$iface_name" != "wlan0" ]]; then
-                # Rename the interface to wlan0
-                ip link set "$iface_name" down
-                ip link set "$iface_name" name wlan0
-                ip link set wlan0 up
-            fi
-        fi
+    # Check if the interface is wireless and USB
+    if [ -d "/sys/class/net/$iface_name/wireless" ] && [[ "$(readlink -f /sys/class/net/$iface_name/device/subsystem)" == *"usb"* ]]; then
+        # Append the interface name to the list
+        matching_interfaces+="$iface_name "
     fi
 done
 
@@ -57,15 +46,20 @@ current_wfb_nics=$(cat /etc/default/wifibroadcast 2>/dev/null)
 # Compare and update if different
 if [[ "$current_wfb_nics" != "$new_wfb_nics" ]]; then
     echo "$new_wfb_nics" >/etc/default/wifibroadcast
+    
     if [[ "$AGGREGATOR_ENABLED" == true ]]; then
-           echo "$(date) [$TYPE]: Restarting wfb-cluster-node service" >>"$LOG_FILE"
+        systemctl restart wfb-cluster-node &
     else
-           echo "$(date) [$TYPE]: Restarting wifibroadcast service" >>"$LOG_FILE"
+        systemctl restart wifibroadcast &
     fi
+
     # Log to file if --log argument was provided
     if [[ -n "$LOG_FILE" ]]; then
-        echo "$(date) [$TYPE]: Network interface $ACTION $INTERFACE" >>"$LOG_FILE"
-        echo "$(date) [$TYPE]: $new_wfb_nics" >>"$LOG_FILE"
-        echo "$(date) [$TYPE]: Restarting Wifibroadcast service" >>"$LOG_FILE"
+        echo "$(date) [$TYPE]: Network interface updated: $new_wfb_nics" >>"$LOG_FILE"
+        if [[ "$AGGREGATOR_ENABLED" == true ]]; then
+            echo "$(date) [$TYPE]: Restarting wfb-cluster-node service" >>"$LOG_FILE"
+        else
+            echo "$(date) [$TYPE]: Restarting wifibroadcast service" >>"$LOG_FILE"
+        fi
     fi
 fi
