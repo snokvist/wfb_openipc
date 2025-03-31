11defc8dd4a77ef089491c12b912a114679be1b8
snokvist <joakim.snokvist@gmail.com>
2025-03-31 20:25:03 +0200

    Update utils.c

--- autopilot/utils.c
+++ autopilot/utils.c
@@ -35,7 +35,7 @@ float get_parameter_from_system(const char *param, float current_value, int verb
     }
     if (fgets(buffer, sizeof(buffer), fp) != NULL) {
         if (strncmp(buffer, "OK", 2) == 0) {
-            float val = atof(buffer + 3); // Assume output "OK <value>"
+            float val = atof(buffer + 3);
             pclose(fp);
             return val;
         } else {
@@ -92,5 +92,34 @@ int get_rc_channel_value(const mavlink_rc_channels_override_t *rc, int channel) 
         case 17: return rc->chan17_raw;
         case 18: return rc->chan18_raw;
         default: return -1;
+    }
+}
+
+speed_t parse_baudrate(const char *str) {
+    long baud = atol(str);
+    switch (baud) {
+        case 50: return B50;
+        case 75: return B75;
+        case 110: return B110;
+        case 134: return B134;
+        case 150: return B150;
+        case 200: return B200;
+        case 300: return B300;
+        case 600: return B600;
+        case 1200: return B1200;
+        case 1800: return B1800;
+        case 2400: return B2400;
+        case 4800: return B4800;
+        case 9600: return B9600;
+        case 19200: return B19200;
+        case 38400: return B38400;
+        case 57600: return B57600;
+        case 115200: return B115200;
+#ifdef B230400
+        case 230400: return B230400;
+#endif
+        default:
+            fprintf(stderr, "Unsupported baud rate: %ld. Defaulting to 115200.\n", baud);
+            return B115200;
     }
 }
