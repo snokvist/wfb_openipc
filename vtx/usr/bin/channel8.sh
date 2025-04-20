2e2f197ea6f18f35f14970ec9a2e99f170f63434
snokvist <joakim.snokvist@gmail.com>
2025-04-20 07:46:36 +0200

    Update channel8.sh

--- vtx/usr/bin/channel8.sh
+++ vtx/usr/bin/channel8.sh
@@ -2,23 +2,39 @@
 
 if [ "$1" -ge 950 ]  && [ "$1" -le 1050 ]; then
     # position 1 (around 1000)
+    echo "1"
 elif [ "$1" -ge 1150 ] && [ "$1" -le 1250 ]; then
     # position 2 (around 1200)
+    echo "1"
 elif [ "$1" -ge 1350 ] && [ "$1" -le 1450 ]; then
     # position 3 (around 1400)
-elif [ "$1" -ge 1550 ] && [ "$1" -le 1650 ]; then
-    # position 4 (around 1600)
-elif [ "$1" -ge 1750 ] && [ "$1" -le 1850 ]; then
-    # position 5 (around 1800)
-elif [ "$1" -ge 1950 ] && [ "$1" -le 2050 ]; then
-    # position 6 (around 2000)
-    channel=165
-    wifibroadcast cli -s .wireless.channel $channel
-    wifibroadcast stop ;wifibroadcast stop; sleep 1;  wifibroadcast start
-    #sed -i "s/^wifi_channel =.*/wifi_channel = $channel/" /etc/wifibroadcast.cfg
-fi
-
-echo "CPU:&C &B temp:&T &L30 &G8 &F30" >/tmp/MSPOSD.msg
-
+    channel=40 
+    echo "CPU:&C &B temp:&T\nSetting channel: $channel ... please wait ... &L30 &G8 &F30" >/tmp/MSPOSD.msg
+    sleep 1.2                 
+    iw dev wlan0 set channel $channel
+elif [ "$1" -ge 1550 ] && [ "$1" -le 1650 ]; then                                                         
+    # position 4 (around 1600)                   
+    channel=48                                   
+    echo "CPU:&C &B temp:&T\nSetting channel: $channel ... please wait ... &L30 &G8 &F30" >/tmp/MSPOSD.msg
+    sleep 1.2                                                                                             
+    iw dev wlan0 set channel $channel                                                                     
+elif [ "$1" -ge 1750 ] && [ "$1" -le 1850 ]; then                                                         
+    # position 5 (around 1800)                   
+    channel=140                                  
+    echo "CPU:&C &B temp:&T\nSetting channel: $channel ... please wait ... &L30 &G8 &F30" >/tmp/MSPOSD.msg
+    sleep 1.2                                                                                             
+    iw dev wlan0 set channel $channel                                                                     
+elif [ "$1" -ge 1950 ] && [ "$1" -le 2050 ]; then                                                         
+    # position 6 (around 2000)       
+    channel=161                      
+    echo "CPU:&C &B temp:&T\nSetting channel: $channel ... please wait ... &L30 &G8 &F30" >/tmp/MSPOSD.msg
+    sleep 1.2                                                             
+    iw dev wlan0 set channel $channel                                     
+fi                                                                        
+                                                                          
+      
+sleep 2
+echo "CPU:&C &B temp:&T\nChannel: $channel &L30 &G8 &F30" >/tmp/MSPOSD.msg
+ 
 exit 1
 **
