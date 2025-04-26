abb86e1f2733bcc189bc9761595fad5bda3e6cd2
snokvist <joakim.snokvist@gmail.com>
2025-04-26 17:00:04 +0200

    Update channel8.sh

--- vtx/usr/bin/channel8.sh
+++ vtx/usr/bin/channel8.sh
@@ -27,24 +27,28 @@ elif [ "$1" -ge 1350 ] && [ "$1" -le 1450 ]; then
     echo "CPU:&C &B temp:&T\nSetting channel: $channel ... please wait ... &L30 &G8 &F30" >/tmp/MSPOSD.msg
     sleep 1.2                                    
     iw dev wlan0 set channel $channel HT20
+    tx_manager.sh set_tx_power 10 --mcs $mcs
 elif [ "$1" -ge 1550 ] && [ "$1" -le 1650 ]; then
     # position 4 (around 1600)
     channel=104                                                                                           
     echo "CPU:&C &B temp:&T\nSetting channel: $channel ... please wait ... &L30 &G8 &F30" >/tmp/MSPOSD.msg
     sleep 1.2                                    
     iw dev wlan0 set channel $channel HT20
+    tx_manager.sh set_tx_power 10 --mcs $mcs
 elif [ "$1" -ge 1750 ] && [ "$1" -le 1850 ]; then
     # position 5 (around 1800)
     channel=124
     echo "CPU:&C &B temp:&T\nSetting channel: $channel ... please wait ... &L30 &G8 &F30" >/tmp/MSPOSD.msg
     sleep 1.2 
     iw dev wlan0 set channel $channel HT20
+    tx_manager.sh set_tx_power 10 --mcs $mcs
 elif [ "$1" -ge 1950 ] && [ "$1" -le 2050 ]; then
     # position 6 (around 2000)
     channel=165
     echo "CPU:&C &B temp:&T\nSetting channel: $channel ... please wait ... &L30 &G8 &F30" >/tmp/MSPOSD.msg
     sleep 1.2
     iw dev wlan0 set channel $channel HT20
+    tx_manager.sh set_tx_power 10 --mcs $mcs
 fi
 
 sleep 2
