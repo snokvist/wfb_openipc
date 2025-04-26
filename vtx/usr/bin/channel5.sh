f2f9a01480d847b3bd0afc8d04a2e1a9f596d19f
snokvist <joakim.snokvist@gmail.com>
2025-04-26 16:28:40 +0200

    Update channel5.sh

--- vtx/usr/bin/channel5.sh
+++ vtx/usr/bin/channel5.sh
@@ -3,10 +3,12 @@ echo "Running logic on msposd channel 5"
 
 if [ "$1" -lt 1200 ]; then
     echo "Disarm detected, setting low power"
-    iw wlan0 set txpower fixed 100
+    #iw wlan0 set txpower fixed 100
+    tx_manager.sh set_tx_power 0 --mcs $mcs
 elif [ "$1" -gt 1800 ]; then
      echo "ARM detected, setting high power"
-     iw wlan0 set txpower fixed 2750
+     #iw wlan0 set txpower fixed 2750
+     tx_manager.sh set_tx_power 10 --mcs $mcs
 fi
     ##TX ON ARM END
 
