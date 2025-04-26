e2414938ca6643a88cbbbd1981df8f556e57fa18
snokvist <joakim.snokvist@gmail.com>
2025-04-26 16:28:05 +0200

    Update channel8.sh

--- vtx/usr/bin/channel8.sh
+++ vtx/usr/bin/channel8.sh
@@ -6,8 +6,10 @@ if [ "$1" -ge 950 ]  && [ "$1" -le 1050 ]; then
     mcs=2
     bw=20
     curl -s 'http://localhost/api/v1/set?video0.bitrate=2888'
-    wfb_tx_cmd 8000 set_radio -B $bw -G short -S 1 -L 1 -M $mcs
+    #wfb_tx_cmd 8000 set_radio -B $bw -G long -S 1 -L 1 -M $mcs
+    tx_manager.sh set_tx_power 10 --mcs $mcs
     sleep 1
+    
     curl -s 'http://localhost/api/v1/set?video0.bitrate=6666'
 elif [ "$1" -ge 1150 ] && [ "$1" -le 1250 ]; then
     # position 2 (around 1200)
@@ -15,7 +17,8 @@ elif [ "$1" -ge 1150 ] && [ "$1" -le 1250 ]; then
     mcs=4
     bw=20
     curl -s 'http://localhost/api/v1/set?video0.bitrate=2800'            
-    wfb_tx_cmd 8000 set_radio -B $bw -G short -S 1 -L 1 -M $mcs
+    #wfb_tx_cmd 8000 set_radio -B $bw -G long -S 1 -L 1 -M $mcs
+    tx_manager.sh set_tx_power 10 --mcs $mcs
     sleep 1
     curl -s 'http://localhost/api/v1/set?video0.bitrate=11333'
 elif [ "$1" -ge 1350 ] && [ "$1" -le 1450 ]; then
