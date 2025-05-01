f2f700d3c15edd0a4747b46c62d03c4bda7cadb1
snokvist <joakim.snokvist@gmail.com>
2025-05-01 13:58:09 +0200

    Update radxa_bridge.sh

--- gs/radxa_bridge.sh
+++ gs/radxa_bridge.sh
@@ -1,7 +1,7 @@
 nmcli con add type bridge ifname br0 \
        ipv4.addresses 192.168.2.20/24 \
-       ipv4.gateway 192.168.2.1 \
-       ipv4.dns 192.168.2.1 \
+       ipv4.gateway 192.168.2.30 \
+       ipv4.dns 192.168.2.30 \
        ipv4.method manual autoconnect yes
 
 nmcli con add type bridge-slave ifname eth0 master br0 autoconnect yes
